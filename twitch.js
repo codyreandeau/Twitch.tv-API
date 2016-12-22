ab52015528d2d7c236d2fabd9fe57a02ad119377
Cody Reandeau <codyreandeau@yahoo.com>
2016-12-22 00:39:16 -0500

    Added function to access twitch api. Added loop to print out the status and name in each person in the list. multiple css changes.

---
+++ twitch.js
@@ -0,0 +1,23 @@
+$(function() {
+  var users = ["FreeCodeCamp", "Syndicate", "Test_Channel"];
+  
+  for (var i = 0; i < users.length; i++) {
+    (function(i) {
+    $.getJSON("https://api.twitch.tv/kraken/streams/"+users[i]+"?client_id=5j0r5b7qb7kro03fvka3o8kbq262wwm&callback=?", function(data) {
+      
+      var status, game = '';
+      
+      if (data.stream == null) { 
+        status='offline';
+      } else {
+        status='online';
+        game = data.stream.game;
+      }
+      
+      $('.stream').append("<div class='subcontainer'><div class='move'><a href='http://www.twitch.tv/" + users[i] + "' target='_blank'>" + users[i] + "</div><span class="+status+">"+status+"</span><div><div>"+game+"</div></a></div>");
+        
+    });
+   })(i);
+  }
+  console.log(status);
+});
