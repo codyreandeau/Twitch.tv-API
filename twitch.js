53cb8c56f12b29c808285e97c270c21856916101
Cody Reandeau <codyreandeau@yahoo.com>
2016-12-23 21:59:34 -0500

    Added images for users. Added more media queries. Added a class called game for the current game streaming.

--- twitch.js
+++ twitch.js
@@ -1,5 +1,6 @@
 $(function() {
   var users = ["FreeCodeCamp", "Syndicate", "Test_Channel"];
+  var images = ["https://ucarecdn.com/357ea30f-33e2-4db2-acc2-719c055891f6/","https://static-cdn.jtvnw.net/jtv_user_pictures/syndicate-profile_image-03bf1d6ad0025f86-300x300.png", "https://s3-us-west-2.amazonaws.com/web-design-ext-production/p/Glitch_474x356.png"]
   
   for (var i = 0; i < users.length; i++) {
     (function(i) {
@@ -14,7 +15,7 @@ $(function() {
         game = data.stream.game;
       }
       
-      $('.stream').append("<div class='subcontainer'><div class='picture'></div><div class='move'><a href='http://www.twitch.tv/" + users[i] + "' target='_blank'>" + users[i] + "</div><span class="+status+">"+status+"</span><div><div>"+game+"</div></a></div>");
+      $('.stream').append("<div class='subcontainer'><img src ="+images[i]+" class='picture'/><div class='move'><a href='http://www.twitch.tv/" + users[i] + "' target='_blank'>" + users[i] + "</div><span class="+status+">"+status+"</span><div><div><div class='game'>"+game+"</div></div></a></div>");
         
     });
    })(i);
